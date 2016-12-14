3d11cc9ef18d9166ef882bac384a3594d6df82fb
Denis Zhbankov <denis.zhbankov@gmail.com>
2016-12-14 13:51:11 +0300

    solve quaddouble

--- quaddouble/index.test.js
+++ quaddouble/index.test.js
@@ -1,17 +1,39 @@
-import quaddouble from './index';
+const quaddouble = require('./index');
 
-test('(45568411115, 11223344) -> true', () => {
-  expect(quaddouble(45568411115, 11223344)).toBe(true);
+describe('required', () => {
+
+  test('(45568411115, 11223344) -> true', () => {
+    expect(quaddouble(45568411115, 11223344)).toBe(true);
+  });
+
+  test('(11112344445, 442253) -> true', () => {
+    expect(quaddouble(11112344445, 442253)).toBe(true);
+  });
+
+  test('(12222345, 123452) -> false', () => {
+    expect(quaddouble(12222345, 123452)).toBe(false);
+  });
+
+  test('(12345, 12345) -> false', () => {
+    expect(quaddouble(12345, 12345)).toBe(false);
+  });
 });
 
-test('(11112344445, 442253) -> true', () => {
-  expect(quaddouble(11112344445, 442253)).toBe(true);
-});
+describe('custom', () => {
 
-test('(12222345, 123452) -> false', () => {
-  expect(quaddouble(12222345, 123452)).toBe(false);
-});
+  test('(4556841.1115, .11223344) -> false', () => {
+    expect(quaddouble(4556841.1115, .11223344)).toBe(false);
+  });
 
-test('(12345, 12345) -> false', () => {
-  expect(quaddouble(12345, 12345)).toBe(false);
+  test('(121122111222, 0) -> false', () => {
+    expect(quaddouble(121122111222, 0)).toBe(false);
+  });
+
+  test('(0000, 00) -> false', () => {
+    expect(quaddouble(0000, 00)).toBe(false);
+  });
+
+  test('(0b100001, 0b1001) -> false', () => {
+    expect(quaddouble(0b100001, 0b1001)).toBe(false);
+  });
 });
